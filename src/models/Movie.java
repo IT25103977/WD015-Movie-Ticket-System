b5db99ad9efbbbae7d6dad42599b80292b28fe67
IT25100157 <it25100157@my.sliit.lk>
2026-05-02 17:33:27 +0530

    movie information

--- src/models/Movie.java
+++ src/models/Movie.java
@@ -1,11 +1,56 @@
 package models;
 
 public class Movie {
-    int Id;
+    int Id;  //attributes
     String Title;
     String genre;
     String director;
     double price;
     int totalSeats;
     String status;
+
+    public Movie() { //empty constructor
+
+    }
+
+    public Movie(String title, int id, String genre, String director, double price, int totalSeats) { // full constructor
+        Title = title;
+        Id = id;
+        this.genre = genre;
+        this.director = director;
+        this.price = price;
+        this.totalSeats = totalSeats;
+    }
+
+    //getters
+    public int getId() {
+        return Id;
+    }
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getTotalSeats() {
+        return totalSeats;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    //setters
+
 }
