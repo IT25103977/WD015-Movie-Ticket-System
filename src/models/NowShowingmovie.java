a839e819d878b23735d1a3930ddae4cdf5d07dbc
IT25100157 <it25100157@my.sliit.lk>
2026-05-02 22:13:44 +0530

    now showing movie information

--- src/models/NowShowingmovie.java
+++ src/models/NowShowingmovie.java
@@ -1,5 +1,38 @@
 package models;
 
-public class NowShowingmovie {
+public class NowShowingmovie extends Movie{
+    private String showTime; //attributes
+    private int theatreNumber;
+
+    public NowShowingmovie() { //empty constructor
+
+    }
+
+    public NowShowingmovie(String title, int id, String genre, String director, double price, int totalSeats, String showTime, int theatreNumber) { //parameterized cnstructor
+        super(title, id, genre, director, price, totalSeats);
+        this.showTime = showTime;
+        this.theatreNumber = theatreNumber;
+    }
+
+    //getter
+
+    public String getShowTime() {
+        return showTime;
+    }
+
+    public int getTheatreNumber() {
+        return theatreNumber;
+    }
+
+    //setter
+
+    public void setShowTime(String showTime) {
+        this.showTime = showTime;
+    }
+
+    public void setTheatreNumber(int theatreNumber) {
+        this.theatreNumber = theatreNumber;
+    }
+
 
 }
