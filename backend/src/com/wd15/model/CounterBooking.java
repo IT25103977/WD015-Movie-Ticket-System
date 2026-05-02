c5ca3c9216d8cba52b828902f27dce21781980dc
IT25102452 <it25102452@my.sliit.lk>
2026-05-02 22:56:57 +0530

    Implement Booking,Online booking and Counter booking classes

--- backend/src/com/wd15/model/CounterBooking.java
+++ backend/src/com/wd15/model/CounterBooking.java
@@ -1,4 +1,55 @@
 package com.wd15.model;
 
-public class CounterBooking {
+public class CounterBooking extends Booking{
+    private String staffId;
+    private int counterNo;
+    private String paymentType;
+
+    //default constructor
+    public CounterBooking(Long id, Long userId, Long movieId, Long seatId, String customerName, String movieTitle, String seatNumber, double totalPrice, String status) {
+        super(id, userId, movieId, seatId, customerName, movieTitle, seatNumber, totalPrice, status);
+    }
+
+    //parameterized constructor
+    public CounterBooking(Long id, Long userId, Long movieId, Long seatId, String customerName, String movieTitle, String seatNumber, double totalPrice, String status, String paymentType, String staffId, int counterNo) {
+        super(id, userId, movieId, seatId, customerName, movieTitle, seatNumber, totalPrice, status);
+        this.paymentType = "Cash";
+        this.staffId = staffId;
+        this.counterNo = counterNo;
+    }
+    //getter and setter methods
+    public String getStaffId() {
+        return staffId;
+    }
+
+    public void setStaffId(String staffId) {
+        this.staffId = staffId;
+    }
+
+    public int getCounterNo() {
+        return counterNo;
+    }
+
+    public void setCounterNo(int counterNo) {
+        this.counterNo = counterNo;
+    }
+
+    public String getPaymentType() {
+        return paymentType;
+    }
+
+    public void setPaymentType(String paymentType) {
+        this.paymentType = paymentType;
+    }
+    // method to Print Ticket
+    public void printPhysicalTicket() {
+        System.out.println("Printing ticket for " + getMovieTitle() + "...");[cite: 1]
+        System.out.println("Issued by Staff: " + this.staffId + " at Counter: " + this.counterNumber);
+    }
+
+    // Method to cancel the online booking
+    public void cancelBooking() {
+        super.cancelBooking(); // Set status to cancelled[cite: 1]
+        System.out.println("Please collect your cash refund from Counter " + this.counterNumber);
+    }
 }
