6b007e7e22f847b1b8c65c52d971b898a2fadb94
Thevinu-S-dev <thevinu.s.2005@gmail.com>
2026-05-05 10:52:19 +0530

    Add User Class

--- backend/src/com/wd15/model/User.java
+++ backend/src/com/wd15/model/User.java
@@ -1,4 +1,10 @@
 package com.wd15.model;
 
 public class User {
+    public long id;
+    public String name;
+    public String email;
+    public String password;
+    public String role;
+    public String phoneNumber;
 }
