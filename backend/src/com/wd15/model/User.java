326788054819747bf8d45e0fdd1bf46c01fca0be
Thevinu-S-dev <thevinu.s.2005@gmail.com>
2026-05-05 12:02:59 +0530

    Updating User Class

--- backend/src/com/wd15/model/User.java
+++ backend/src/com/wd15/model/User.java
@@ -1,10 +1,84 @@
 package com.wd15.model;
 
 public class User {
-    public long id;
-    public String name;
-    public String email;
-    public String password;
-    public String role;
-    public String phoneNumber;
+    // Attributes
+    private long id;
+    private String name;
+    private String email;
+    private String password;
+    private String role;
+    private String phoneNumber;
+
+    //Default Constructor
+    public User() {
+    }
+
+    //Parameterized Constructor
+    public User(long id, String name, String email, String password, String role, String phoneNumber) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+        this.phoneNumber = phoneNumber;
+    }
+
+    //All the getters
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    //All the setters
+    public void setId(int userId) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    // Method
+    public void displayInfo() {
+        System.out.println("User ID: " +id);
+        System.out.println("Name: " +name);
+        System.out.println("Email: " +email);
+        System.out.println("Phone Number: " +phoneNumber);
+    }
+    
 }
