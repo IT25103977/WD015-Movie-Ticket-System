d2aa651b5a2c4e3f6cdff358b7473182b09fae54
IT25103661 <IT25103661@my.sliit.lk>
2026-05-05 14:05:31 +0530

    Add Seat Class for movie ticket System

--- backend/src/com/wd15/model/Seat.java
+++ backend/src/com/wd15/model/Seat.java
@@ -1,4 +1,14 @@
 package com.wd15.model;
 
 public class Seat {
+
+    private String Seat_number;
+  private String row_Letter;
+    private String seat_Type;
+    private boolean isBooked;
+    private int seat_Column;
+
+
+
+
 }
