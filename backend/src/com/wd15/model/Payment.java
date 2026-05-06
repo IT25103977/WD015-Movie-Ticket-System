df951a8812c3e200e420899201f6c8a6c95a64a6
TharuUP <computerhacker566@gmail.com>
2026-05-06 22:11:29 +0530

    First Startup

--- backend/src/com/wd15/model/Payment.java
+++ backend/src/com/wd15/model/Payment.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 
 public class Payment {
 
+    //Attributes
     private long id;
     private long    bookingId;
     private double amount;
@@ -13,6 +14,7 @@ public class Payment {
     private String status;
     private String transactionId;
 
+    //Constructor
     public Payment (){
 
     }
@@ -27,6 +29,57 @@ public class Payment {
      this.transactionId = "TXN" + System.currentTimeMillis();
     }
 
-    
+    //set the getters
+    public long getId(){
+        return id;
+    }
+
+    public long getBookingId(){
+        return bookingId;
+    }
+
+    public double getAmount(){
+        return amount;
+    }
+
+    public String getPaymentMethod(){
+        return paymentMethod;
+    }
+
+    public String getPaymentDate(){
+        return paymentDate;
+    }
+
+    public String getStatus(){
+        return status;
+    }
+
+    public String getTransactionId(){
+        return transactionId;
+    }
+
+    // set the setters
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void refund(){
+        this.status = "Refunded";
+    }
+
+    public void displayInfo(){
+        System.out.println("Payment ID: " + id);
+        System.out.println("Booking ID: " + bookingId);
+        System.out.println("Amount: " + amount);
+        System.out.println("Payment Method: " + paymentMethod);
+        System.out.println("Payment Date: " + paymentDate);
+        System.out.println("Status: " + status);
+        System.out.println("Transaction ID: " + transactionId);
+    }
+
 
 }
