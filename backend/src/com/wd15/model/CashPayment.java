1d81657a4f6b37ca4b32dfca1522c3ba2c0a2cd9
TharuUP <computerhacker566@gmail.com>
2026-05-06 22:35:00 +0530

    Cash Payment

--- backend/src/com/wd15/model/CashPayment.java
+++ backend/src/com/wd15/model/CashPayment.java
@@ -1,4 +1,41 @@
 package com.wd15.model;
 
-public class CashPayment {
+public class CashPayment extends Payment{
+
+    //Attributes
+    private double amountReceived;
+    private double changeGiven;
+
+    //Constructor
+    public CashPayment(){
+
+    }
+
+    public CashPayment(long bookingId, double amount, String paymentMethod, double amountReceived){
+        super(bookingId, amount, paymentMethod);
+
+        this.amountReceived = amountReceived;
+        this.changeGiven = amountReceived - amount;
+
+    }
+
+    //Set the getters
+    public double getAmountReceived(){
+        return amountReceived;
+
+    }
+
+    public double getChangeGiven(){
+        return changeGiven;
+    }
+
+    //override
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+
+        System.out.println("Amount Recived: " + amountReceived);
+        System.out.println("Change Givern: " + changeGiven);
+    }
 }
