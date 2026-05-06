8e41f0dbcdc3208e1a3cca0a69424fc0e513e6e8
TharuUP <computerhacker566@gmail.com>
2026-05-06 22:00:14 +0530

    First Startup

--- backend/src/com/wd15/model/Payment.java
+++ backend/src/com/wd15/model/Payment.java
@@ -1,7 +1,32 @@
 package com.wd15.model;
 
+import java.security.PrivateKey;
+import java.time.LocalDateTime;
+
 public class Payment {
-    public static void main(String[] args){
-        System.out.println("Hello");
+
+    private long id;
+    private long    bookingId;
+    private double amount;
+    private String paymentMethod;
+    private String paymentDate;
+    private String status;
+    private String transactionId;
+
+    public Payment (){
+
     }
+
+    public Payment(long bookingId, double amount, String paymentMethod, String paymentDate){
+     this.bookingId = bookingId;
+     this.amount = amount;
+     this.paymentMethod = paymentMethod;
+
+     this.paymentDate = LocalDateTime.now().toString();
+     this.status = "Success";
+     this.transactionId = "TXN" + System.currentTimeMillis();
+    }
+
+    
+
 }
