1a9e982750c35fbccf495fd368ccdbc33a99956b
TharuUP <computerhacker566@gmail.com>
2026-05-07 23:32:00 +0530

    main app

--- backend/src/com/wd15/main/MainApp.java
+++ backend/src/com/wd15/main/MainApp.java
@@ -1,7 +1,24 @@
 package com.wd15.main;
 
+import com.wd15.model.CardPayment;
+import com.wd15.model.CashPayment;
+
 public class MainApp {
     public static void main(String[] args) {
-        System.out.println("Movie Ticket System Started");
+        CardPayment card = new CardPayment(
+                101,2500,"card","4521","visa"
+        );
+
+        card.displayInfo();
+
+        CashPayment cash = new CashPayment(
+                102,3000,"cash", 2000
+        );
+
+        cash.displayInfo();
+
+        card.refund();
+
+        System.out.println("Payment After Refund: " + card.getStatus());
     }
 }
