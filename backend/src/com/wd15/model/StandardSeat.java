baca501123c4a2e365dd6d716602ba985f7d5d97
IT25103661 <IT25103661@my.sliit.lk>
2026-05-07 12:05:11 +0530

    Add StandeardSeat model

--- backend/src/com/wd15/model/StandardSeat.java
+++ backend/src/com/wd15/model/StandardSeat.java
@@ -1,4 +1,25 @@
 package com.wd15.model;
 
-public class StandardSeat {
+public class StandardSeat extends Seat {
+
+    private String rowSection;
+
+    // Constructor
+    public StandardSeat(Long movieId, String seatNumber, String rowLetter, int seatColumn,
+                        String rowSection) {
+        super(movieId, seatNumber, rowLetter, seatColumn, "Standard");
+        this.rowSection = rowSection;
+    }
+
+    // Getter
+    public String getRowSection() {
+        return rowSection;
+    }
+
+    //  display
+    public void displayStatus() {
+        String status = isBooked() ? "Booked" : "Available";
+        System.out.println("STANDARD - " + getSeatNumber() + " [" + rowSection + "]: " + status);
+    }
+
 }
