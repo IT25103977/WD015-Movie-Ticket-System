0c7ce68c81b8bc2154ce34b4589f96fdf3ab1d3f
Thevinu-S-dev <thevinu.s.2005@gmail.com>
2026-05-17 15:59:31 +0530

    Updating LoginRequest.java

---
+++ backend/src/main/java/com/wd15/dto/LoginRequest.java
@@ -0,0 +1,19 @@
+package main.java.com.wd15.dto;
+
+public class LoginRequest {
+
+    //Attributes
+    private String email;
+    private String password;
+
+    //Getters
+    public String getEmail() { return email; }
+
+    public String getPassword() { return password; }
+
+    //Setters
+    public void setEmail(String email) { this.email = email; }
+
+    public void setPassword(String password) { this.password = password; }
+
+}
