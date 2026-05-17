bd56b6cfee0ab312360a4db7ee9da65d16e3a4ee
Thevinu-S-dev <thevinu.s.2005@gmail.com>
2026-05-17 15:57:25 +0530

    Minor fixes

--- backend/src/main/java/com/wd15/model/Person.java
+++ backend/src/main/java/com/wd15/model/Person.java
@@ -38,45 +38,4 @@ public abstract class Person extends BaseEntity {
     }
 
 
-}package com.wd15.model;
-
-import jakarta.persistence.MappedSuperclass;
-
-@MappedSuperclass
-public abstract class Person extends BaseEntity {
-
-    //Attributes
-    private String name;
-    private String email;
-
-    //Default Constructor
-    public Person() {
-    }
-
-    //Parameterized Constructor
-    public Person(String name, String email) {
-        this.name = name;
-        this.email = email;
-    }
-
-    //Getters
-    public String getName() {
-        return name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    //Setters
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-
 }
