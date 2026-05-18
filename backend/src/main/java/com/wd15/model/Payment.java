7a19d9a4b3a180bfbda3a2a83f0c032de92fcf00
TharuUP <computerhacker566@gmail.com>
2026-05-18 23:30:24 +0530

    Final Update

--- backend/src/main/java/com/wd15/model/Payment.java
+++ backend/src/main/java/com/wd15/model/Payment.java
@@ -4,17 +4,32 @@ import jakarta.persistence.Embeddable;
 
 @Embeddable
 public class Payment {
+
     private String method;
     private String status;
     private int amount;
 
+    private String cardHolderName;
+    private String cardNumber;
+    private String expiryDate;
+    private String cvv;
+
     public Payment() {
     }
 
-    public Payment(String method, String status, int amount) {
+    public Payment(String method, String status, int amount,
+            String cardHolderName,
+            String cardNumber,
+            String expiryDate,
+            String cvv) {
+
         this.method = method;
         this.status = status;
         this.amount = amount;
+        this.cardHolderName = cardHolderName;
+        this.cardNumber = cardNumber;
+        this.expiryDate = expiryDate;
+        this.cvv = cvv;
     }
 
     public String getMethod() {
@@ -39,5 +54,37 @@ public class Payment {
 
     public void setAmount(int amount) {
         this.amount = amount;
+    }
+
+    public String getCardHolderName() {
+        return cardHolderName;
+    }
+
+    public void setCardHolderName(String cardHolderName) {
+        this.cardHolderName = cardHolderName;
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public String getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setExpiryDate(String expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+
+    public String getCvv() {
+        return cvv;
+    }
+
+    public void setCvv(String cvv) {
+        this.cvv = cvv;
     }
 }
